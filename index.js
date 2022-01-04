2397825bbb9e2ea0058cdd6a1998e049f62cdd6f
Elvan H <elvanhd@gmail.com>
2022-01-04 09:03:08 +0700

    membuat aplikasi express sederhana

---
+++ index.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
